ca6a6d32c59c6b5a30ca5c5e3c5d29efac3f49da
wangmin168999 <1129198270@qq.com>
2018-06-29 16:55:05 +0800

    first commit

---
+++ scripts/test_network.py
@@ -0,0 +1,12 @@
+class TestNetwork:
+    def test_01(self):
+        assert 1
+
+    def test_02(self):
+        assert 1
+
+    def test_03(self):
+        assert 0
+
+    def test_04(self):
+        assert 1
